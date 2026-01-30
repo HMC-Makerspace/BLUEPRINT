4803558909fee014202109760746aec2b4e166b2
Wil <wil@gmx.com>
2026-01-29 17:11:15 -0800

    Fix start.bat typo and ensure `cache` directory is populated.

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Mozilla Firefox\firefox.exe" -private http://127.0.0.1:500
+"C:\Program Files\Mozilla Firefox\firefox.exe" -private http://127.0.0.1:5000
 start "LFP" /B python -m flask run
